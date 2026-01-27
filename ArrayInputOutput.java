a9a248de6f627f4ba658c6e6bb9101485938d973
KARTIK KUMAR <158415361+kartiksingh0@users.noreply.github.com>
2026-01-27 20:58:32 +0530

    Add files via upload

---
+++ ArrayInputOutput.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+class ArrayInputOutput{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = sc.nextInt();
+
+
+        int arr[] = new int[size];
+        System.out.println("Enter the array elements");
+        for(int i =0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are:- ");
+        for(int i=0; i<size;i++){
+            System.out.print(arr[i]+ " ");
+        }
+
+        sc.close();
+    }
+}
